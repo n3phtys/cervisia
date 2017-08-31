bb2fcc0a3323082bc982ac3c3a41dbc735c3dfdc
nephtys <christopher.kaag@gmail.com>
2017-08-31 22:16:08 +0200

    fixed, compiles now and opens gtk window

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,36 @@ use gtk::prelude::*;
 #[macro_use] extern crate closet;
 extern crate blrustix;
 
+use std::ops::DerefMut;
 use std::cell::RefCell;
+use std::rc::Rc;
+use blrustix::rustix_backend::*;
+use blrustix::persistencer::*;
 
 fn main() {
     println!("Hello, world!");
 
-    let backend = RefCell::new(blrustix::default::build_transient_backend()); //TODO: clone() also clones the value, this prevents the kind of use we want
+    let mut backend = Rc::new(RefCell::new(blrustix::build_transient_backend()));
+
+    {
+        //prepare transient backup a little bit
+        let mut bl2 = &*Rc::get_mut(&mut backend).unwrap();
+        let mut bl3 = bl2.borrow_mut();
+        let bl : &mut RustixBackend<TransientPersister> = bl3.deref_mut();
+
+        bl.create_user("Gruin".to_string());
+        bl.create_user("Vall".to_string());
+        bl.create_user("rad(i)".to_string());
+
+        bl.create_item("Club Mate".to_string(), 100, Some("without alcohol".to_string()));
+        bl.create_item("Pils".to_string(), 95, Some("Beer".to_string()));
+        bl.create_item("Whiskey".to_string(), 1200, Some("Liquor".to_string()));
+
+        bl.purchase(0, 2, 12345678u32);
+
+    }
+
+
 
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -28,6 +52,7 @@ fn main() {
         Inhibit(false)
     });
 
+    window.show_all();
 
     gtk::main();
 
