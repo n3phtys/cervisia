d9ab9cce42927917d6f08916b5ce3079c15fb75e
nephtys <christopher.kaag@gmail.com>
2017-09-11 21:20:48 +0200

    added tree listview as from gtk-rs example, but with users listed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate gtk;
 
 use gtk::prelude::*;
+use gtk::{
+    CellRendererText, Label, ListStore, Orientation, TreeView, TreeViewColumn,
+    Window, WindowPosition, WindowType
+};
 #[macro_use] extern crate closet;
 extern crate blrustix;
 
@@ -9,6 +13,12 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use blrustix::rustix_backend::*;
 use blrustix::persistencer::*;
+use blrustix::datastore::*;
+use std::cell::*;
+use std::ops::*;
+use std::borrow::*;
+use gtk::ScrolledWindow;
+use gtk::Adjustment;
 
 fn main() {
     println!("Hello, world!");
@@ -24,6 +34,10 @@ fn main() {
         bl.create_user("Gruin".to_string());
         bl.create_user("Vall".to_string());
         bl.create_user("rad(i)".to_string());
+
+        for i in 0..997 {
+            bl.create_user("GenUser #".to_string() + &i.to_string());
+        }
 
         bl.create_item("Club Mate".to_string(), 100, Some("without alcohol".to_string()));
         bl.create_item("Pils".to_string(), 95, Some("Beer".to_string()));
@@ -52,8 +66,103 @@ fn main() {
         Inhibit(false)
     });
 
+
+    // Creating a vertical layout to place both tree view and label in the window.
+    let vertical_layout = gtk::Box::new(Orientation::Vertical, 0);
+
+    // Creation of the label.
+    let label = Label::new(None);
+
+    let tree = create_and_setup_view();
+
+
+    let model = {
+
+
+        let bl2 = &*Rc::get_mut(&mut backend).unwrap();
+        let bl3 = bl2.borrow();
+        let bl : &RustixBackend<TransientPersister> = bl3.deref();
+
+        create_and_fill_model(&bl.datastore)
+    };
+
+    // Setting the model into the view.
+    tree.set_model(Some(&model));
+    // Adding the view to the layout.
+
+    let scroll = {
+        let hadjustment = None;
+        let vadjustment = None;
+        let wdw = ScrolledWindow::new(hadjustment, vadjustment);
+        wdw
+    };
+
+    vertical_layout.add(&scroll);
+
+    {
+        scroll.add(&tree);
+    }
+
+    // Same goes for the label.
+    vertical_layout.add(&label);
+
+    // The closure responds to selection changes by connection to "::cursor-changed" signal,
+    // that gets emitted when the cursor moves (focus changes).
+    tree.connect_cursor_changed(move |tree_view| {
+        let selection = tree_view.get_selection();
+        if let Some((model, iter)) = selection.get_selected() {
+            // Now getting back the values from the row corresponding to the
+            // iterator `iter`.
+            //
+            // The `get_value` method do the conversion between the gtk type and Rust.
+            label.set_text(&format!("Hello '{}' from row {}",
+                                    model.get_value(&iter, 1).get::<String>().unwrap(),
+                                    model.get_value(&iter, 0).get::<u32>().unwrap()));
+        }
+    });
+
+
+
+
+    // Adding the layout to the window.
+    window.add(&vertical_layout);
+
     window.show_all();
 
     gtk::main();
 
+}
+
+
+fn create_and_setup_view() -> TreeView {
+    // Creating the tree view.
+    let tree = TreeView::new();
+
+    tree.set_headers_visible(false);
+    // Creating the two columns inside the view.
+    append_column(&tree, 0);
+    append_column(&tree, 1);
+    tree
+}
+
+
+fn append_column(tree: &TreeView, id: i32) {
+    let column = TreeViewColumn::new();
+    let cell = CellRendererText::new();
+
+    column.pack_start(&cell, true);
+    // Association of the view's column with the model's `id` column.
+    column.add_attribute(&cell, "text", id);
+    tree.append_column(&column);
+}
+
+fn create_and_fill_model(datastore: &Datastore) -> ListStore {
+    // Creation of a model with two rows.
+    let model = ListStore::new(&[u32::static_type(), String::static_type()]);
+
+    // Filling up the tree view.
+    for (_, (id, user)) in datastore.users.iter().enumerate() {
+        model.insert_with_values(None, &[0, 1], &[&id, &user.username]);
+    }
+    model
 }
